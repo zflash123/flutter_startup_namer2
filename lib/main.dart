3cdb8bf264387733ebdd5c4980298905d6a59979
Zaed Abdullah <z4ed.thalib123@gmail.com>
2022-03-01 08:50:16 +0700

    Add love button in each row

--- lib/main.dart
+++ lib/main.dart
@@ -26,7 +26,8 @@ class RandomWords extends StatefulWidget {
 
 class _RandomWordsState extends State<RandomWords> {
   final _suggestions = <WordPair>[];
-  final _biggerFont = const TextStyle(fontSize: 18);
+  final _saved = <WordPair>[];
+  final _biggerFont = TextStyle(fontSize: 18);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -53,10 +54,16 @@ class _RandomWordsState extends State<RandomWords> {
     );
   }
   Widget _buildRow(WordPair pair) {
+    final alreadySaved = _saved.contains(pair);
     return ListTile(
       title: Text(
         pair.asPascalCase,
         style: _biggerFont,
+      ),
+      trailing: Icon(
+        alreadySaved ? Icons.favorite : Icons.favorite_border,
+        color: alreadySaved ? Colors.red : null,
+        semanticLabel: alreadySaved ? 'Remove from saved' : 'Save',
       ),
     );
   }
